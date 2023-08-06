019733759ee70341e6376ac84900ee9e636443d3
Jaianee <126702934+Jaaiane@users.noreply.github.com>
2023-08-05 23:45:07 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,53 @@
+let nome = [
+    "Maria",
+    "João",
+    "Pedro",
+    "Ana",
+    "Joaquim",
+    "Marcos",
+    "Pedro",
+    "Marcos",
+    "Matheus", 
+    "Henrique"
+]
+
+let idade = [
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29
+]
+
+let corFav = [
+    "Verde",
+    "Vermelho",
+    "Laranja",
+    "Rosa",
+    "Cinza",
+    "Azul",
+    "Marrom",
+    "Preto",
+    "Branco",
+    "Amarelo"
+]
+
+console.log(nome)
+console.log(idade)
+console.log(corFav)
+
+idade [5] = 23
+corFav [3] = "Roxo"
+
+console.log(nome)
+console.log(idade)
+console.log(corFav)
+
+
+
+    
