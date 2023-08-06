468315a46e35d10397ca1ea99e7342e5499d2736
Jaianee <126702934+Jaaiane@users.noreply.github.com>
2023-08-06 18:28:36 -0300

    Add files via upload

---
+++ FilaListaPilha.js
@@ -0,0 +1,36 @@
+//Remoção com Fila
+
+const fila = [3, 7, 9, 1, 0]
+
+fila.shift()
+fila.shift()
+fila.shift()
+fila.shift()
+fila.shift()
+
+console.log("Removido da fila:", fila)
+
+console.log("---------------------------")
+
+//Remoção com Lista
+
+const lista = [3, 7, 9, 1, 0]
+
+lista.splice(0,5)
+
+console.log("Removido da lista:", lista)
+
+console.log("---------------------------")
+
+//Remoção com Pilha
+
+const pilha = [3, 7, 9, 1, 0]
+
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+
+console.log("Removido da pilha:", pilha)
+
