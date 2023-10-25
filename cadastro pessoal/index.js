0ff6a672533d0d181338d17eafc47b55e2ccd5b7
Jaiane Pereira <pereirajaiane43@gmail.com>
2023-10-25 18:56:24 -0300

    new changes

---
+++ cadastro pessoal/index.js
@@ -0,0 +1,9 @@
+let nome = "Jaiane";
+let salario = 2500;
+let idade = 19;
+let possuiDiploma = true;
+
+console.log("Nome:" + nome)
+console.log("Salário:" + salario)
+console.log("Idade:" + idade)
+console.log("Possui Diploma?" + (possuiDiploma ? "Sim" : "Não"));
